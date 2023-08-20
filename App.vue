3cd5d10ab9d7fc75b28e2316d51dd9d0ae4974fa
Code_yks <48611034+yksCode@users.noreply.github.com>
2023-08-20 19:32:07 +0800

    property
    property

---
+++ App.vue
@@ -0,0 +1,30 @@
+<script>
+	import config from '@/app.config.js'
+	export default {
+		methods: {
+
+		},
+		// 监听 - 页面404
+		onPageNotFound:function(e) {
+			uni.redirectTo({
+				url: config.error.url
+			})
+		},
+		onLaunch: function() {
+			if(config.debug) console.log('App Launch')
+			let that = this;
+		},
+		onShow: function() {
+			if(config.debug) console.log('App Show')
+		},
+		onHide: function() {
+			if(config.debug) console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+  @import "./common/css/app.scss";
+</style>
